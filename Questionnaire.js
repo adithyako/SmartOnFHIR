be6c7c9210b2acb6226d974b2e0fdb2061e71a0b
Pearl Joby <pjoby@montgomerycollege.edu>
2024-06-12 12:29:56 -0400

    changes

--- Questionnaire.js
+++ Questionnaire.js
@@ -3,4 +3,7 @@ window.onload = main;
 function main() {
 
     document.querySelector("#Q76513-1").innerHTML = "How hard is it for you to pay for the very basics like food, housing, medical care, and heating?";
+
+    document.querySelector("#Q69861-3").innerHTML = "Because of a physical, mental, or emotional condition, do you have difficulty doing errands alone such as visiting a physician's office or shopping?";
+
 }
