18e3623e80f39c3c6a76aa12c114c3419d29fb1f
adithyako <38380981+adithyako@users.noreply.github.com>
2024-06-12 11:00:59 -0400

    added form

---
+++ Questionnaire.js
@@ -0,0 +1,6 @@
+window.onload = main;
+
+function main() {
+
+    document.querySelector("#Q76513-1").innerHTML = "How hard is it for you to pay for the very basics like food, housing, medical care, and heating?";
+}
