8d4d58c4a07a61c62d15ae38b543b16dafcdcd26
adithyako <38380981+adithyako@users.noreply.github.com>
2024-06-12 11:12:47 -0400

    made changes

--- Questionnaire.js
+++ Questionnaire.js
@@ -3,4 +3,5 @@ window.onload = main;
 function main() {
 
     document.querySelector("#Q76513-1").innerHTML = "How hard is it for you to pay for the very basics like food, housing, medical care, and heating?";
+    document.querySelector("#Q96780-2").innerHTML = "Do you want help finding or keeping work or a job?";
 }
