9e8459dc978b98ae5aa5d8561b07b4819842fb1f
adithyako <38380981+adithyako@users.noreply.github.com>
2024-06-24 12:06:10 -0400

    Testing home page and submit

--- Questionnaire.js
+++ Questionnaire.js
@@ -112,6 +112,10 @@ app.get('/index', function (req, res) {
 //     res.render("app", variables);
 // });
 
+app.get('/home', function (req, res) {
+    res.render('home');
+});
+
 app.get("/app", (req, res) => {
     smart(req, res).ready().then(client => handler(client, res));
     const variables = { formToAdd: jsonList };
