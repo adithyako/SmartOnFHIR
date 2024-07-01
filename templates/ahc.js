56536afe9e2e1f825a672b54dc9ba344e8aef395
soorxa <soorya405@gmail.com>
2024-07-01 12:12:55 -0400

    testing again

---
+++ templates/ahc.js
@@ -0,0 +1,5 @@
+src="https://lhcforms-static.nlm.nih.gov/lforms-versions/34.0.0/webcomponent/assets/lib/zone.min.js"
+
+src="https://lhcforms-static.nlm.nih.gov/lforms-versions/34.0.0/webcomponent/lhc-forms.js"
+
+src="https://lhcforms-static.nlm.nih.gov/lforms-versions/34.0.0/fhir/R4/lformsFHIR.min.js"
